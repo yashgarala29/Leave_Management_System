cc76c2711aa3b8a7cf28c50ea2413a98c5d058b3
yashgarala29 <yashgarala29@gmail.com>
2021-01-23 09:20:50 +0530

    leave approve and reject done

--- Leave_Management_System/Controllers/HODController.cs
+++ Leave_Management_System/Controllers/HODController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Leave_Management_System.Models.Class;
+using Leave_Management_System.Models.Context;
+using Leave_Management_System.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +13,59 @@ namespace Leave_Management_System.Controllers
 {
     public class HODController : Controller
     {
-        public IActionResult Index()
+        private readonly LeaveDbContext _context;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        public HODController(LeaveDbContext context, UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
         {
-            return View();
+            _context = context;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ListOfLeaveRequest()
+        {
+            var hodDeparment =  _context.AllUser.Where(x => x.Email == User.Identity.Name).FirstOrDefault();
+            var requestedleave =  _context.LeaveHistory.Include(l => l.AllUser)
+                .Where(x => (x.AllUser.Deparment==hodDeparment.Deparment && x.AllUser.id!=hodDeparment.id))
+                .ToList();
+            var allrequest = new List<ListOfLeaveRequestHOD>();
+            for(int i=0;i<requestedleave.Count;i++)
+            {
+                var listre = new ListOfLeaveRequestHOD
+                {
+                    id = requestedleave[i].leave_id,
+                    FirstName = requestedleave[i].AllUser.Name,
+                    LastName = requestedleave[i].AllUser.LastName,
+                    LeaveEndTill = requestedleave[i].EndTill,
+                    LeaveReason = requestedleave[i].LeaveReason,
+                    LeaveStartFrome = requestedleave[i].StartFrome,
+                    NoOfDay = requestedleave[i].NoOfDay,
+                    username = requestedleave[i].AllUser.Email,
+                    HODStatus = Enum.GetNames(typeof(Status)).ToList()
+                };
+                allrequest.Add(listre);
+            }
+            return View(allrequest);
+        }
+        [HttpPost]
+        public async Task<JsonResult> AjaxMethod(string name,string leave_id)
+        {
+            int leave_id_int = int.Parse(leave_id);
+            var leave=_context.LeaveHistory.Where(x => x.leave_id == leave_id_int).FirstOrDefault();
+            leave.LeaveStatus = name;
+            
+            //try
+            //{
+                _context.Update(leave);
+                await _context.SaveChangesAsync();
+            //}
+            //catch (Exception e)
+            //{
+            //    throw;
+            //}
+            return Json(leave) ;
         }
     }
 }
---
+++ Leave_Management_System/Models/ViewModel/ListOfLeaveRequestHOD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management_System.Models.ViewModel
+{
+    public class ListOfLeaveRequestHOD
+    {
+        public int id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Username")]
+        public string username { get; set; }
+        [Display(Name = "Start Frome")]
+        public DateTime LeaveStartFrome { get; set;}
+        [Display(Name = "End Till")]
+        public DateTime LeaveEndTill { get; set; }
+        [Display(Name ="No Of Day")]
+        public int NoOfDay { get; set; }
+        [Display(Name = "Status")]
+        public List<string> HODStatus { get; set; }
+        [Display(Name = "Leave Reason")]
+        public string LeaveReason { get; set; }
+
+    }
+}
